f921f44131af04abbc055be97bad6defc065a368
Max Ellisen <max@ezic.io>
2023-08-22 15:43:12 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Max Ellisen on 22.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
